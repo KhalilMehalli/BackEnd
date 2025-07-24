f0c70732fdc2e2fd4d16965efbd53039d666d1cd
Khalil <khalilmehalli@gmail.com>
2025-07-25 00:49:36 +0200

    back for chat fix

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();  // Load .env into process.env     
 const express    = require("express");
+const http    = require("http");
 const cors       = require("cors");
+const { Server } = require("socket.io");
 
 const contactRoutes = require("./routes/contactForm");
 
@@ -30,6 +32,36 @@ app.get('/', (_req, res) => {
   console.log("ping received ")
 });
 
+//
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:4173",
+      "https://mohamed-khalil-mehalli-etu.pedaweb.univ-amu.fr"
+    ],
+    methods: ["GET", "POST"]
+  }
+});
+
+
+//
+io.on("connection", (socket) => {
+  console.log("Nouvelle connexion :", socket.id);
+
+  socket.on("chat message", (msg) => {
+    socket.broadcast.emit("chat message", msg);   // diffuse à tous
+    console.log(msg)
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Déconnexion :", socket.id);
+  });
+});
+
+
 // Start server 
 const PORT =  process.env.PORT || 3000;
-app.listen(PORT, () => {console.log("Start server on port", PORT)});
+server.listen(PORT, () => {console.log("Start server on port", PORT)});
